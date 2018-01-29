cc62b53a920ed15e7f7d8e4ec9dccb080b52f369
Mustapha Manjoura <Mustapha.Manjoura@bd.com>
2018-01-29 10:37:56 +0000

    Initial Commit for Qor-example

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/qor/admin"
+	"github.com/qor/publish2"
+	"github.com/qor/qor"
+	"github.com/qor/qor-example/app/account"
+	adminapp "github.com/qor/qor-example/app/admin"
+	"github.com/qor/qor-example/app/api"
+	"github.com/qor/qor-example/app/enterprise"
+	"github.com/qor/qor-example/app/home"
+	"github.com/qor/qor-example/app/orders"
+	"github.com/qor/qor-example/app/pages"
+	"github.com/qor/qor-example/app/products"
+	"github.com/qor/qor-example/app/static"
+	"github.com/qor/qor-example/config"
+	"github.com/qor/qor-example/config/application"
+	"github.com/qor/qor-example/config/auth"
+	"github.com/qor/qor-example/config/bindatafs"
+	"github.com/qor/qor-example/config/db"
+	_ "github.com/qor/qor-example/config/db/migrations"
+	"github.com/qor/qor-example/utils"
+	qorutils "github.com/qor/qor/utils"
+)
+
+func main() {
+	cmdLine := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	compileTemplate := cmdLine.Bool("compile-templates", false, "Compile Templates")
+	cmdLine.Parse(os.Args[1:])
+
+	var (
+		Router = chi.NewRouter()
+		Admin  = admin.New(&admin.AdminConfig{
+			SiteName: "QOR DEMO",
+			Auth:     auth.AdminAuth{},
+			DB:       db.DB.Set(publish2.VisibleMode, publish2.ModeOff).Set(publish2.ScheduleMode, publish2.ModeOff),
+		})
+		Application = application.New(&application.Config{
+			Router: Router,
+			Admin:  Admin,
+			DB:     db.DB,
+		})
+	)
+
+	utils.AddFuncMapMaker(auth.Auth.Config.Render)
+
+	Router.Use(func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			// for demo, don't use this for your production site
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			handler.ServeHTTP(w, req)
+		})
+	})
+
+	Router.Use(middleware.RealIP)
+	Router.Use(middleware.Logger)
+	Router.Use(middleware.Recoverer)
+	Router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			var (
+				tx         = db.DB
+				qorContext = &qor.Context{Request: req, Writer: w}
+			)
+
+			if locale := qorutils.GetLocale(qorContext); locale != "" {
+				tx = tx.Set("l10n:locale", locale)
+			}
+
+			ctx := context.WithValue(req.Context(), qorutils.ContextDBName, publish2.PreviewByDB(tx, qorContext))
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	})
+
+	Application.Use(api.New(&api.Config{}))
+	Application.Use(adminapp.New(&adminapp.Config{}))
+	Application.Use(home.New(&home.Config{}))
+	Application.Use(products.New(&products.Config{}))
+	Application.Use(account.New(&account.Config{}))
+	Application.Use(orders.New(&orders.Config{}))
+	Application.Use(pages.New(&pages.Config{}))
+	Application.Use(enterprise.New(&enterprise.Config{}))
+	Application.Use(static.New(&static.Config{
+		Prefixs: []string{"/system"},
+		Handler: qorutils.FileServer(http.Dir(filepath.Join(config.Root, "public"))),
+	}))
+	Application.Use(static.New(&static.Config{
+		Prefixs: []string{"javascripts", "stylesheets", "images", "dist", "fonts", "vendors"},
+		Handler: bindatafs.AssetFS.FileServer(http.Dir("public"), "javascripts", "stylesheets", "images", "dist", "fonts", "vendors"),
+	}))
+
+	if *compileTemplate {
+		bindatafs.AssetFS.Compile()
+	} else {
+		fmt.Printf("Listening on: %v\n", config.Config.Port)
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), Application.NewServeMux()); err != nil {
+			panic(err)
+		}
+	}
+}
